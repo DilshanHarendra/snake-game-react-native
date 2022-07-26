03b812dc0c7b11334be466aeafb45ca724d6aa7e
DilshanHarendra <harendracripto@gmail.com>
2022-07-26 14:35:03 +0530

    create main file

---
+++ source/SnakeGame.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+
+import DefaultLayout from './layouts/DefaultLayout';
+function SnakeGame() {
+  return (
+    <View>
+      <DefaultLayout />
+    </View>
+  );
+}
+
+export default SnakeGame;
