e87b638ae6a1ba2d3b1eda00f6564fe3f4c2072e
DilshanHarendra <harendracripto@gmail.com>
2022-07-28 00:51:34 +0530

    connect store & bind data

--- source/components/SingleUserRow.js
+++ source/components/SingleUserRow.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity} from 'react-native';
 import UserAvatar from 'react-native-user-avatar';
 import tw from 'twrnc';
-function SingleUserRow() {
+import {useNavigate} from 'react-router-native';
+import {setCurrentUser} from '../store/actions/user';
+
+function SingleUserRow({user}) {
+  let navigate = useNavigate();
+
+  function clickHandler() {
+    setCurrentUser(user);
+    navigate('/player', {replace: true});
+  }
+
   return (
-    <View style={tw`flex-row items-center py-2 hover:bg-gray-200`}>
-      <UserAvatar size={40} name="John Doe" />
-      <Text style={tw`ml-3 text-lg`}>Jhone Doe</Text>
-    </View>
+    <TouchableOpacity onPress={clickHandler}>
+      <View style={tw`flex-row items-center py-2  hover:bg-gray-200`}>
+        <UserAvatar size={40} style={tw`w-10`} name={user.name} />
+        <View style={tw`ml-3`}>
+          <Text style={tw`text-lg font-semibold`}>{user.name}</Text>
+          <Text style={tw`text-gray-500`}>Score {user.score}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   );
 }
 export default SingleUserRow;
